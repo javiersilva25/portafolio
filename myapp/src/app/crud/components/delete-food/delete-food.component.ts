857226cd6043b706a03181e5acb0299de9956b4a
Javier <javie.silvav@duocuc.cl>
2025-04-04 11:10:57 -0300

    Estructura principal
    Adiciones a crud, se crearán ramas de desarrollo y testing

--- myapp/src/app/crud/components/delete-food/delete-food.component.ts
+++ myapp/src/app/crud/components/delete-food/delete-food.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-delete-food',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delete-food.component.scss'],
   standalone: false
 })
-export class DeleteFoodComponent  implements OnInit {
+export class DeleteFoodComponent implements OnInit {
+  alimentos: any[] = [];
+  alimentoSeleccionadoId: string = '';
 
-  constructor() { }
+  constructor(private api: ApiService, private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.cargarAlimentos();
+  }
 
+  cargarAlimentos() {
+    this.api.getAllFoods().subscribe(
+      (data: any) => {
+        this.alimentos = data;
+      },
+      (error) => {
+        console.error('Error al cargar alimentos', error);
+      }
+    );
+  }
+
+  eliminarAlimento() {
+    if (this.alimentoSeleccionadoId) {
+      this.api.deleteFood(this.alimentoSeleccionadoId).subscribe(
+        (response) => {
+          console.log('Alimento eliminado exitosamente', response);
+          this.cargarAlimentos(); // refresca la lista
+          this.router.navigate(['/macros/deleteFood']); // o quedarte donde estás
+        },
+        (error) => {
+          console.error('Error al eliminar el alimento', error);
+        }
+      );
+    } else {
+      console.error('No se ha seleccionado ningún alimento');
+    }
+  }
 }
