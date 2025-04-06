7b11a138212e49371c3b1c1ad9f9580de61277dc
Javier <javie.silvav@duocuc.cl>
2025-04-06 15:13:25 -0400

    Update models.py

--- fastapi-backend/models.py
+++ fastapi-backend/models.py
@@ -10,3 +10,9 @@ class Food(Base):
     proteinas = Column(Integer)
     carbohidratos = Column(Integer)
     grasas = Column(Integer)
+
+class Exercise(Base):
+    __tablename__ = "exercises"
+
+    id = Column(String, primary_key=True, index=True)
+    nombre = Column(String, index=True)
