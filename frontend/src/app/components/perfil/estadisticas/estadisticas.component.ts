e5d1f7014e0edf2972de9aa04ed1133f1474bc76
IgnacioPuelles <139293045+IgnacioPuelles@users.noreply.github.com>
2025-05-14 21:22:24 -0400

    getMedidas en estadisticas.component

--- frontend/src/app/components/perfil/estadisticas/estadisticas.component.ts
+++ frontend/src/app/components/perfil/estadisticas/estadisticas.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import type { EChartsCoreOption } from 'echarts/core';
 import { TooltipComponent, LegendComponent } from 'echarts/components';
+import { PerfilService } from 'src/app/services/perfil.service';
 
 @Component({
   selector: 'app-estadisticas',
@@ -11,49 +12,65 @@ import { TooltipComponent, LegendComponent } from 'echarts/components';
 })
 export class EstadisticasComponent  implements OnInit {
   options: EChartsCoreOption = {};
+  medidas: any[] = [];
+  fechas: string[] = [];
+  valores: number[] = [];
 
-  constructor(private modalController: ModalController) { }
+  constructor(private modalController: ModalController,
+    private perfilService: PerfilService
+  ) { }
 
   ngOnInit(): void {
-    const xAxisData = [];
-    const data1 = [];
-    const data2 = [];
+    this.perfilService.getMedidas().subscribe({
+      next: (data) => {
+        this.medidas = data;
 
-    for (let i = 0; i < 100; i++) {
-      xAxisData.push('category' + i);
-      data1.push((Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
-      data2.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
-    }
+        this.medidas.forEach(medida => {
+          this.fechas.push(medida.fecha);
+          this.valores.push(medida.valor);
+        });
 
-    this.options = {
-      legend: {
-        data: ['bar', 'bar2'],
-        align: 'left',
+        
+        // this.medidas = this.agruparMedidasPorNombre(data);
+        console.log(this.medidas); // BORRAR
+        console.log(this.fechas); // BORRAR
+        console.log(this.valores); // BORRAR
+
+        const xAxisData = this.fechas;
+        const data1 = this.valores;
+
+        this.options = {
+          legend: {
+            data: ['bar'],
+            align: 'left',
+          },
+          tooltip: {},
+          xAxis: {
+            data: xAxisData,
+            silent: false,
+            splitLine: {
+              show: false,
+            },
+          },
+          yAxis: [{
+            type: 'value',
+          }],
+          series: [
+            {
+              name: 'bar',
+              type: 'bar',
+              barWidth: '60%',
+              data: data1
+            },
+          ],
+          animationEasing: 'elasticOut',
+          animationDelayUpdate: idx => idx * 5,
+        };
       },
-      tooltip: {},
-      xAxis: {
-        data: xAxisData,
-        silent: false,
-        splitLine: {
-          show: false,
-        },
-      },
-      yAxis: {},
-      series: [
-        {
-          name: 'bar',
-          type: 'bar',
-          data: data1
-        },
-        {
-          name: 'bar2',
-          type: 'bar',
-          data: data2
-        },
-      ],
-      animationEasing: 'elasticOut',
-      animationDelayUpdate: idx => idx * 5,
-    };
+      error: (err) => {
+        console.error('Error al obtener medidas del usuario', err);
+      }
+    });
   }
 
   cancelar() {
