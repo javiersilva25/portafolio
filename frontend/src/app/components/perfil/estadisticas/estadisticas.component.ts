ce51be137df7682601bc8e094f6ca9c86ccf26c9
IgnacioPuelles <139293045+IgnacioPuelles@users.noreply.github.com>
2025-05-14 21:40:01 -0400

    Implementación datos peso de usuario en gráfico

--- frontend/src/app/components/perfil/estadisticas/estadisticas.component.ts
+++ frontend/src/app/components/perfil/estadisticas/estadisticas.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import type { EChartsCoreOption } from 'echarts/core';
-import { TooltipComponent, LegendComponent } from 'echarts/components';
+import { TooltipComponent, LegendComponent } from 'echarts/components'; // Import missing components
 import { PerfilService } from 'src/app/services/perfil.service';
+import { LineChart } from 'echarts/charts';
+
+// Register the components with ECharts
+import * as echarts from 'echarts/core';
+echarts.use([TooltipComponent, LegendComponent, LineChart]); // Register Tooltip and Legend components
 
 @Component({
   selector: 'app-estadisticas',
@@ -10,55 +15,55 @@ import { PerfilService } from 'src/app/services/perfil.service';
   templateUrl: './estadisticas.component.html',
   styleUrls: ['./estadisticas.component.scss']
 })
-export class EstadisticasComponent  implements OnInit {
+export class EstadisticasComponent implements OnInit {
   options: EChartsCoreOption = {};
   medidas: any[] = [];
   fechas: string[] = [];
   valores: number[] = [];
 
-  constructor(private modalController: ModalController,
-    private perfilService: PerfilService
-  ) { }
+  constructor(private modalController: ModalController, private perfilService: PerfilService) { }
 
   ngOnInit(): void {
     this.perfilService.getMedidas().subscribe({
       next: (data) => {
         this.medidas = data;
-
         this.medidas.forEach(medida => {
           this.fechas.push(medida.fecha);
           this.valores.push(medida.valor);
         });
 
-        
-        // this.medidas = this.agruparMedidasPorNombre(data);
+        // Console logs for debugging (remove in production)
         console.log(this.medidas); // BORRAR
         console.log(this.fechas); // BORRAR
         console.log(this.valores); // BORRAR
 
+        // Now, create the chart options
         const xAxisData = this.fechas;
         const data1 = this.valores;
 
         this.options = {
           legend: {
-            data: ['bar'],
+            data: ['Peso (Kg)'],
             align: 'left',
           },
           tooltip: {},
           xAxis: {
             data: xAxisData,
-            silent: false,
-            splitLine: {
-              show: false,
+            axisTick  : {
+              alignWithLabel: true
             },
+            axisLabel: {
+              rotate: -45,  // Rotates labels to make them vertical (sideways)
+              interval: 0  // Ensures that all labels are rotated (optional)
+            }
           },
           yAxis: [{
             type: 'value',
           }],
           series: [
             {
-              name: 'bar',
-              type: 'bar',
+              name: 'Peso (Kg)',
+              type: 'line',
               barWidth: '60%',
               data: data1
             },
